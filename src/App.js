e77785aecd2d54f781cbd4c111790bb0b1d59f45
sanabel <ssssasomostafa123@gmail.com>
2022-10-29 19:29:27 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 
 import './App.css';
-// import Home from './components/Home/home';
-
-import Porfile from './components/profile/Porfile';
-import Longin from './components/login/Longin';
+import Home from './components/Home/home';
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-    {/* <Home /> */}
-    {/*<Porfile />*/} 
-    <Longin/>
-=======
-      {/* <Home /> */}
-      <Porfile />
->>>>>>> 5b962754d65799dfdcc9cb6872b1193f8635540a
+    <Home />
     </>
   );
 }
