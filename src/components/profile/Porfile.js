a33cf0dd3b4e58008eb76c02813918259cb79b55
Dina-Abuo <dynaalkhfajy73@gmail.com>
2022-11-10 12:37:46 +0200

    edit profile

--- src/components/profile/Porfile.js
+++ src/components/profile/Porfile.js
@@ -1,31 +1,61 @@
 import React, { useState } from 'react';
 import HeadProfile from './head/HeadProfile'
-import Nav from './Nav/Nav'
+// import Nav from './Nav/Nav'
+import './profileStyle.css'
 import Posts from './posts/posts'
 import Abouts from './About/Abouts';
 import Friends from './friends/Friends';
 import Photos from './Photos/Photos';
 import Vidoes from './Vidoes/vidoes';
 export default function Porfile() {
-    useState(
-        {
-            Posts: false,
-            Friends: false,
-            Videos: false,
-            photos: false,
-        }
-    )
-    const handleClik = () => {
-    }
+    const [show, setshow] = useState(true)
+    const handleClik = () => { setshow(!show) }
+
+    const [showAoute, setshowAoute] = useState(false)
+    const handleClikAoute = () => { setshowAoute(!showAoute) }
+
+    const [showFriends, setshowFriends] = useState(false)
+    const handleClikFriends = () => { setshowFriends(!showFriends) }
+
+    const [showPhotos, setshowPhotos] = useState(false)
+    const handleClikPhotosPhotos = () => { setshowPhotos(!showPhotos) }
+
+    const [showVidoes, setshowVidoes] = useState(false)
+    const handleClikVidoes = () => { setshowVidoes(!showVidoes) }
+
     return (
         <>
             <HeadProfile />
-            <Nav />
-            <Posts />
-            <Abouts />
-            <Friends />
-            <Photos />
-            <Vidoes />
+
+            <div className='perent-button '>
+                <div className='col-8   perent-button  mb-3  border-top'>
+                    <button className='class-Nav  me-4 ' onClick={handleClik} >Posts </button>
+                    <button className='class-Nav   me-4 ' onClick={handleClikAoute} >Abouts </button>
+                    <button className='class-Nav   me-4 ' onClick={handleClikFriends}>Friends </button>
+                    <button className='class-Nav   me-4 ' onClick={handleClikPhotosPhotos}>Photos</button>
+                    <button className='class-Nav   me-4 ' onClick={handleClikVidoes}>Vidoes </button>
+                </div>
+            </div>
+
+            {/* <Nav /> */}
+            {
+                show && <Posts />
+            }
+            {
+                showAoute && <Abouts />
+            }
+            {
+                showFriends && <Friends />
+            }
+
+            {
+                showPhotos && <Photos />
+            }
+            {
+                showVidoes && <Vidoes />
+            }
+
+
         </>
     )
 }
