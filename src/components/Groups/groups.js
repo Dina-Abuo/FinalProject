2b29f05bb2e7fd4d172ca4cfc5e1b0a59b197620
sanabel <ssssasomostafa123@gmail.com>
2022-11-10 02:16:25 +0200

    Edits

--- src/components/Groups/groups.js
+++ src/components/Groups/groups.js
@@ -1,10 +1,44 @@
-
+import './groups.css'
+import Post2 from './post2/post2'
+import Post from './post2/post2'
+                                                           
+                                      
 export default function Groups() {
   return (
     <>
-     <div>sanabel</div>
-     <div>Dina</div>
-     <div>Sapry</div>
+    <div className="d-flex col-12">
+     <div className="side1 col-5">
+     <div className = "sidebar">
+                <div>
+                <i className="fa-solid fa-users fa-2x p-3"></i> Mohamed Salah   
+                </div> 
+                  <div>
+                <i className="fa-solid fa-users fa-2x p-3"></i> Freind
+                </div>
+                <div>
+                <i className="fa-solid fa-tv fa-2x p-3"></i> Watch
+                    
+                </div> 
+                <div>
+                <i className="fa-solid fa-users fa-2x p-3"></i> Group
+                </div>
+                <div>
+                <i className="fa-solid fa-shop fa-2x p-3"></i> Marketplace
+                </div>    
+            </div>  
+     </div>
+     <div className=" side2 col-7">
+   
+     <Post 
+                profilePic =" http://sites.duke.edu/wcwp/files/2020/02/Picture1.png"
+                message = "  انا الملك المصري"
+                timestamp = "time..."
+                username = "mo Salah"
+                image = "http://sites.duke.edu/wcwp/files/2020/02/Picture1.png" 
+            />
+     </div>
+   
+     </div>
     </>
   )
 }
--- src/components/Groups/post2/post2.js
+++ src/components/Groups/post2/post2.js
@@ -1,4 +1,54 @@
-import './Post.css'
+// import './post2.css'
+
+
+
+
+
+// export default function Post2(profilePic2, image2, username2, timestamp2, message2) {
+//   return (
+//     <>
+//     <div className = "post">
+//             <div className = "post__top">
+//             <img src={profilePic2} 
+//             class="post__avatar"
+//              alt="Avatar" 
+//              />
+//                 {/* <Avatar
+//                     src = {profilePic}
+//                     className = ""
+//                 /> */}
+//                 <div className = "post__topInfo">
+//                     <h3>{username2}</h3>
+//                     <p>9.45</p>
+//                 </div>
+//             </div>
+//                 <div className = "post__bottom">
+//                     <p>{message2}</p>
+//                 </div>
+//                     <div className = "post__image">
+//                         <img src = {image2} 
+//                             alt = ""
+//                         />
+//                     </div>
+//                     <div className = "post__options">
+//                         {/* <FBReactions /> */}
+//                         <div className = "post__option">
+//                             <i class="far fa-comment i"></i>
+//                             <p>Comment</p>
+//                         </div>
+//                         <div className = "post__option">
+//                             <i class="fas fa-share-square i"></i>
+//                             <p>Share</p>
+//                         </div>
+//                     </div>
+
+//         </div>
+//     </>
+//   )
+// }
+
+
+import './post2.css'
 import React,{useEffect,useState} from 'react'
 
 function Post({ profilePic, image, username, timestamp, message}) {
